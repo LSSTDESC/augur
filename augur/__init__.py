3b6dccbaf9aa546f8d2dcad7eb55cc37734555be
Anze Slosar <slosar@gmail.com>
2020-03-19 09:36:40 -0400

    boilerplate

---
+++ augur/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from .parser import parse
+
+## implement in due time
+generate = lambda config, output, verbose : None
+analyze = lambda config, output, verbose : None
+postprocess = lambda config, output, verbose : None
---
+++ augur/parser.py
@@ -0,0 +1,24 @@
+import importlib
+import yaml
+import jinja2
+
+
+def parse(filename):
+    """Parse a configuration file.
+
+    Parameters
+    ----------
+    filename : str
+        The config file to parse. Should be YAML formatted.
+
+    Returns
+    -------
+    config: dict
+        The raw config file as a dictionary.
+    """
+
+    with open(filename, 'r') as fp:
+        config_str = jinja2.Template(fp.read()).render()
+    config = yaml.load(config_str, Loader=yaml.Loader)
+
+    return config
