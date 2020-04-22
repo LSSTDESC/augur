939398a25ffc60762532c81a0d2cbd0c7c30130e
Anze Slosar <slosar@gmail.com>
2020-04-22 05:24:07 -0400

    test bug

--- augur/tests/conftest.py
+++ augur/tests/conftest.py
@@ -7,6 +7,7 @@ def example_yaml(tmpdir):
     yaml = parse("examples/srd_v1_3x2.yaml")
     yaml['verbose'] = True
     yaml['generate']['two_point']['sacc_file'] = str(tmpdir.join('test.sacc'))
+    yaml['analyze']['two_point']['sacc_data'] = str(tmpdir.join('test.sacc'))
     yaml['analyze']['cosmosis']['output_dir'] = str(tmpdir)
 
     # make fewer parameters to enable fisher to run faster
