c4173b75bf293bd642200363a5a86ab618504148
Javier Sanchez <francs1@uci.edu>
2021-08-30 18:08:31 -0500

    updated conftest

--- augur/tests/conftest.py
+++ augur/tests/conftest.py
@@ -4,7 +4,7 @@ from ..parser import parse
 
 @pytest.fixture
 def example_yaml(tmpdir):
-    yaml = parse("examples/srd_v1_3x2.yaml")
+    yaml = parse("examples/srd_y1_3x2.yaml")
     yaml['verbose'] = True
     yaml['generate']['two_point']['sacc_file'] = str(tmpdir.join('test.sacc'))
     yaml['analyze']['two_point']['sacc_data'] = str(tmpdir.join('test.sacc'))
