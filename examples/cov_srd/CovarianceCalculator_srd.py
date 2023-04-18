48b6966384be690ad962b2308d31e3074ff830bb
Chisa002Gemini <n.e.chisari@uu.nl>
2023-04-18 21:24:06 +0200

    Files to generate SRD-like covariance

---
+++ examples/cov_srd/CovarianceCalculator_srd.py
@@ -0,0 +1,15 @@
+from tjpcov.covariance_calculator import CovarianceCalculator
+from matplotlib import pyplot as plt
+import numpy as np
+import sacc
+
+# In order to use the config file in tests, you need your working directory to be the root folder
+import os
+os.chdir('../')
+
+config_yml = '/nethome/chisa002/TJPCov/tests/data/srd/conf_covariance_gaussian_fsky_fourier_srd.yaml'
+cc = CovarianceCalculator(config_yml)
+cov = cc.get_covariance()
+s = cc.create_sacc_cov(output="srd_3x2pt_Y1_covariance.fits")
+
+exit()
