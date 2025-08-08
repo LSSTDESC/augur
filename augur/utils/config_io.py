518165dcb8cc73f623a3aaaadab626311eecea85
Javier Sanchez <jsanchez@Javiers-MacBook-Air-3.local>
2025-08-08 17:14:31 +0200

    fixed cobaya sampler

--- augur/utils/config_io.py
+++ augur/utils/config_io.py
@@ -1,11 +1,30 @@
+import yaml
+import re
+import os
+
+path_matcher = re.compile(r'.*\$\{([^}^{]+)\}.*')
+
+
+def path_constructor(loader, node):
+    return os.path.expandvars(node.value)
+
+
+class EnvVarLoader(yaml.SafeLoader):
+    pass
+
+
+EnvVarLoader.add_implicit_resolver('!path', path_matcher, None)
+EnvVarLoader.add_constructor('!path', path_constructor)
+
+
 def parse_config(config):
     """
     Utility to parse configuration file
     """
     if isinstance(config, str):
-        import yaml
-        with open(config) as f:
-            config = yaml.safe_load(f)
+        with open(config, 'r') as f:
+            config = f.read()
+        config = yaml.load(config, Loader=EnvVarLoader)
     elif isinstance(config, dict):
         pass
     else:
