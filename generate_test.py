e9284010125a70d3605b7fb1fb250aaf614422b9
paulrogozenski <paulrogozenski@gmail.com>
2025-12-22 15:10:20 -0500

    generation test update, remove diagnostic plotting scripts

--- generate_test.py
+++ generate_test.py
@@ -3,7 +3,6 @@ from augur.analyze import Analyze
 from augur.postprocess import postprocess
 
 
-import numpy as np
 # lk = generate('./examples/srd_y10_3x2.yml', return_all_outputs=False)
 # lk = generate('./examples/srd_y1_3x2.yml', return_all_outputs=False)
 
@@ -14,7 +13,7 @@ import numpy as np
 # lk = generate('./examples/config_test.yml', return_all_outputs=False)
 
 cf = './examples/srd_y1_3x2.yml'
-lk, _,tools, req_params = generate(cf, return_all_outputs=True)
+lk, _, tools, req_params = generate(cf, return_all_outputs=True)
 print(req_params.items())
 ao = Analyze(cf, lk, tools=tools, req_params=req_params, norm_step=False)
 fisher = ao.get_fisher_matrix(method='5pt_stencil')
