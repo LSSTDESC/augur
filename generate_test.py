54be167ab52b464177450abd06a8b50714f2ba30
Javier Sanchez <jsanchez@Mac.lan>
2025-12-22 18:03:06 -0500

    removing postprocess for now

--- generate_test.py
+++ generate_test.py
@@ -1,12 +1,7 @@
 from augur.generate import generate
 from augur.analyze import Analyze
-from augur.postprocess import postprocess
-
 
 cf = './augur/tests/test.yaml'
 lk, _, tools, req_params = generate(cf, return_all_outputs=True)
-print(req_params.items())
 ao = Analyze(cf, lk, tools=tools, req_params=req_params, norm_step=False)
 fisher = ao.get_fisher_matrix(method='5pt_stencil')
-
-postprocess(cf)
