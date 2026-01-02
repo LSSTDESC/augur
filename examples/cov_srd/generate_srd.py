862b1208610121312a52fee88e6a8cbf48494b76
Javier Sanchez <fjavier.sanchez.cosmo@gmail.comn>
2026-01-02 12:29:08 -0500

    small fix trying to address #104 as well

--- examples/cov_srd/generate_srd.py
+++ examples/cov_srd/generate_srd.py
@@ -1,8 +1,10 @@
 from augur.generate import generate
-
-likelihood, sacc_data, tools = generate('/nethome/chisa002/augur/examples/config_test.yml',
-                                        return_all_outputs=True, force_read=True)
-
-sacc_data.save_fits("srd.fits", overwrite=True)
+from pathlib import Path
+import os
+path = Path(__file__).parent
+fpath = os.path.join(path, 'config_test.yml')
+likelihood, sacc_data, tools = generate(fpath, return_all_outputs=True, force_read=True)
+opath = os.path.join(path, 'srd.fits')
+sacc_data.save_fits(opath, overwrite=True)
 
 exit()
