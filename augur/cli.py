eb6e91d9512630b7bc3afeaf4915836c1df5a020
paulrogozenski <paulrogozenski@gmail.com>
2026-04-02 09:56:55 -0400

    location change for cli

---
+++ augur/cli.py
@@ -0,0 +1,47 @@
+"""Command-line interface for Augur."""
+
+import pprint
+
+import click
+
+import augur
+from augur.analyze import Analyze
+from augur.generate import generate
+from augur.postprocess import postprocess
+
+
+@click.command()
+@click.argument("config", type=str)
+@click.option("-v", "--verbose", is_flag=True)
+def run(config, verbose):
+    """Run Augur from a YAML configuration file."""
+    parsed_config = augur.parse(config)
+    if verbose:
+        print("rendered config file:\n", pprint.pformat(parsed_config), flush=True)
+
+    print("Generating fiducial data vector...", flush=True)
+    likelihood, tools, req_params = generate(parsed_config, return_all_outputs=True)
+
+    if "fisher" in parsed_config:
+        print("Computing Fisher matrix...", flush=True)
+        analysis = Analyze(
+            parsed_config,
+            likelihood=likelihood,
+            tools=tools,
+            req_params=req_params,
+        )
+        analysis.get_fisher_matrix()
+        if "gaussian_priors" in parsed_config["fisher"]:
+            analysis.add_gaussian_priors()
+        if "fisher_bias" in parsed_config["fisher"]:
+            analysis.get_fisher_bias()
+
+    if "postprocess" in parsed_config:
+        print("Postprocessing...", flush=True)
+        postprocess(parsed_config)
+
+    print("Done.", flush=True)
+
+
+if __name__ == "__main__":
+    run()
