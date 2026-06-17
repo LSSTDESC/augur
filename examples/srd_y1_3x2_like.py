cbacf7511335772a85b14e76e0f01672092eaa98
paulrogozenski <43966955+paulrogozenski@users.noreply.github.com>
2026-06-17 09:22:58 -0700

    flake8 fix

--- examples/srd_y1_3x2_like.py
+++ examples/srd_y1_3x2_like.py
@@ -1,9 +1,8 @@
 from augur.generate import generate
-import sacc
+
 
 def build_likelihood(_):
-    # S = sacc.Sacc.load_fits('./test_sacc.sacc')
-    like, tools, sys_params = generate('./config_test.yml', return_all_outputs=True)#, use_sacc = S, sacc_path='./test_sacc.sacc')
+    like, tools, sys_params = generate('./config_test.yml', return_all_outputs=True)
     like.reset()
     tools.reset()
     return like
