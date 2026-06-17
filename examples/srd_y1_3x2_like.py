9135ae8bc1f2b1f8025be34646b18767af19201e
paulrogozenski <43966955+paulrogozenski@users.noreply.github.com>
2026-06-17 09:40:12 -0700

    missing output variable

--- examples/srd_y1_3x2_like.py
+++ examples/srd_y1_3x2_like.py
@@ -2,7 +2,7 @@ from augur.generate import generate
 
 
 def build_likelihood(_):
-    like, tools, sys_params = generate('./config_test.yml', return_all_outputs=True)
+    like, S, tools, sys_params = generate('./config_test.yml', return_all_outputs=True)
     like.reset()
     tools.reset()
     return like
